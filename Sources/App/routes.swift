4f121d729a056b1f01fcad2b5661a2b556d925c3
彭军涛 <pengjuntao@macbookpro.local>
2018-05-15 21:54:51 +0800

    add some message

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,11 +12,23 @@ public func routes(_ router: Router) throws {
             return acronym.save(on: req)
         }
     }
-
+    router.get("hello",String.parameter) { (req) -> String in
+        let name = try req.parameters.next(String.self)
+        
+        return "Hello,\(name)!"
+    }
+    
+    router.post(InfoData.self,at: "info") { (req,data) in
+        return "Hello \(data.name)!"
+    }
 
     // Example of configuring a controller
 //    let todoController = TodoController()
 //    router.get("todos", use: todoController.index)
 //    router.post("todos", use: todoController.create)
 //    router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+struct InfoData: Content {
+    let name: String
 }
